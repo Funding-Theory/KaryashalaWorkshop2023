eb4b4a9e29b1212c4a673eb4d2d9c9cd38f221c0
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-07-13 01:59:00 +0530

    updated logo size

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,35 +1,29 @@
 <template>
-  <nav class="bg-white border-gray-200">
-    <div
-      class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
-    >
-      <a href="https://www.nibmg.ac.in" class="flex items-center">
-        <img class="h-8" alt="NIBMG Logo" src="@/assets/images/logo1.png" />
-      </a>
+    <nav class="bg-white border-gray-200">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="https://www.nibmg.ac.in" class="flex items-center">
+                <img class="h-12" alt="NIBMG Logo" src="@/assets/images/logo1.png" />
+            </a>
 
-      <a href="https://acceleratevigyan.gov.in" class="flex items-center">
-        <img
-          class="h-8"
-          alt="Accelerate Vigyan Logo"
-          src="@/assets/images/logo2.png"
-        />
-      </a>
+            <a href="https://acceleratevigyan.gov.in" class="flex items-center">
+                <img class="h-12" alt="Accelerate Vigyan Logo" src="@/assets/images/logo2.png" />
+            </a>
 
-      <a href="https://serb.gov.in" class="flex items-center">
-        <img class="h-8" alt="SERB Logo" src="@/assets/images/logo3.png" />
-      </a>
-    </div>
-  </nav>
+            <a href="https://serb.gov.in" class="flex items-center">
+                <img class="h-12" alt="SERB Logo" src="@/assets/images/logo3.png" />
+            </a>
+        </div>
+    </nav>
 </template>
 
 <script>
 export default {
-  data: () => ({}),
-  components: {},
-  methods: {},
-  mounted() {
-    this.$nextTick(() => {})
-  },
+    data: () => ({}),
+    components: {},
+    methods: {},
+    mounted() {
+        this.$nextTick(() => {})
+    },
 }
 </script>
 
