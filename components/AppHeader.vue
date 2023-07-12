d6e6aca5437409e5aac1480002b74fbc2ff5182f
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-07-13 01:55:26 +0530

    updated

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,9 +1,9 @@
 <template>
     <header class="bg-gray-50">
         <div class="lg:mx-52 px-4 py-8 sm:px-6 lg:px-8">
-            <!-- <div class="max-w-lg overflow-x-hidden"> -->
-            <img src="@/assets/images/Media_1.png" alt="" class="rounded-lg w-96" />
-            <!-- </div> -->
+            <div class="max-w-lg overflow-x-hidden">
+                <img src="@/assets/images/Media_1.png" alt="" class="rounded-lg w-96" />
+            </div>
             <div class="mt-8">
                 <h1 class="text-2xl font-bold text-gray-600 sm:text-3xl">Welcome Participants</h1>
                 <h1 class="text-2xl font-bold text-gray-900 sm:text-3xl">SERB High-End Workshop (KARYASHALA)</h1>
